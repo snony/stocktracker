073544ba93356a56eef36e1f5a6d60b4d024cbad
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-09-12 11:53:32 +0100

    getting the close data

--- src/components/company/chart.js
+++ src/components/company/chart.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import getStock from './../../api';
+
+const initState = {
+    display: 'closed',
+    closed:[],
+    open: [],
+    high: [],
+    low:[]
+}
 
 class Chart extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = initState;
+    }
+    componentDidUpdate(prevProps) {
+        if (this.props.symbol !== prevProps.symbol) {
+            this.getChartData();
+        }
+    }
 
+    getChartData = () => {
+        const symbol = this.props.symbol;
+        getStock(symbol, "chart/ytd?filter=date,close").then((chartData) => {
+            this.setState({ closed: JSON.stringify(chartData) });
+        });
+    }
     render() {
+        const listData = this.state.closed;
+        //console.log(listData);
         return (
-            <div><h3>Historical Data Chart with Filters</h3></div>
+
+            <div><h3>Historical Data Chart with Filters</h3>
+                {listData}
+            </div>
         );   
     }
 }
