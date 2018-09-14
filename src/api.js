293e10e7b0c868de92bf87e11b185990b38f9f22
Toby Li <toby.li@weareadaptive.com>
2018-09-14 10:31:25 +0100

    Use prettier to format code

--- src/api.js
+++ src/api.js
@@ -1,64 +1,63 @@
-const host = 'https://api.iextrading.com/1.0/';
+const host = "https://api.iextrading.com/1.0/";
 
-const fetchAndUnpack = url => fetch(url).then(data => data.json())
+const fetchAndUnpack = url => fetch(url).then(data => data.json());
 
 export const getChart = (symbol, dateFilter, priceFilter) => {
-    const url = `${host}stock/${symbol}/chart/${dateFilter}?filter=date,${priceFilter}`;
-    return fetchAndUnpack(url)
-}
+  const url = `${host}stock/${symbol}/chart/${dateFilter}?filter=date,${priceFilter}`;
+  return fetchAndUnpack(url);
+};
 
 export const getNews = symbol => {
-    const url = `${host}stock/${symbol}/news/last/5`;
-    return fetchAndUnpack(url)
-}
+  const url = `${host}stock/${symbol}/news/last/5`;
+  return fetchAndUnpack(url);
+};
 
 export const getOverview = symbol => {
-    const url = `${host}stock/${symbol}/company`;
-    return fetchAndUnpack(url)
-}
+  const url = `${host}stock/${symbol}/company`;
+  return fetchAndUnpack(url);
+};
 
 export const getPeers = symbol => {
-    const url = `${host}stock/${symbol}/peers`;
-    return fetchAndUnpack(url)
-}
+  const url = `${host}stock/${symbol}/peers`;
+  return fetchAndUnpack(url);
+};
 
 export const getStats = symbol => {
-    const url = `${host}stock/${symbol}/stats`;
-    return fetchAndUnpack(url)
-}
+  const url = `${host}stock/${symbol}/stats`;
+  return fetchAndUnpack(url);
+};
 
 export const getPrevious = symbol => {
-    const url = `${host}stock/${symbol}/previous`;
-    return fetchAndUnpack(url)
-}
+  const url = `${host}stock/${symbol}/previous`;
+  return fetchAndUnpack(url);
+};
 export const getOhlc = symbol => {
-    const url = `${host}stock/${symbol}/ohlc`;
-    return fetchAndUnpack(url)
-}
+  const url = `${host}stock/${symbol}/ohlc`;
+  return fetchAndUnpack(url);
+};
 export const getQuote = symbol => {
-    const url = `${host}stock/${symbol}/quote`;
-    return fetchAndUnpack(url)
-}
+  const url = `${host}stock/${symbol}/quote`;
+  return fetchAndUnpack(url);
+};
 
-const keyStatsServices = [getStats, getPrevious, getOhlc, getQuote]
-
+const keyStatsServices = [getStats, getPrevious, getOhlc, getQuote];
 
 const extractData = ([stats, previous, ohlc, quote]) => ({
-    earningsPerShare: stats.latestEPS,
-    dividendYield: stats.dividendYield,
-    previousClose: previous.close,
-    open: ohlc.open.price,
-    close: ohlc.close.price,
-    dayRange: ohlc.high - ohlc.low,
-    volume: quote.latestVolume,
-    avgTotalVolume: quote.avgTotalVolume,
-    peRatio: quote.peRatio,
-    marketCap: quote.marketCap,
-    weekRange52: quote.week52High - quote.week52Low,
-})
+  earningsPerShare: stats.latestEPS,
+  dividendYield: stats.dividendYield,
+  previousClose: previous.close,
+  open: ohlc.open.price,
+  close: ohlc.close.price,
+  dayRange: ohlc.high - ohlc.low,
+  volume: quote.latestVolume,
+  avgTotalVolume: quote.avgTotalVolume,
+  peRatio: quote.peRatio,
+  marketCap: quote.marketCap,
+  weekRange52: quote.week52High - quote.week52Low
+});
 
 export const getKeyStats = symbol => {
-    return Promise
-        .all(keyStatsServices.map(service => service(symbol)))
-        .then(extractData)
-}
+  return Promise.all(keyStatsServices.map(service => service(symbol))).then(
+    extractData
+  );
+};
--- src/components/company/stats.js
+++ src/components/company/stats.js
@@ -1,56 +1,64 @@
-import React from 'react';
-import { getKeyStats } from './../../api'
+import React from "react";
+import { getKeyStats } from "./../../api";
 
 class StatsContainer extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            keyStats: null
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyStats: null
+    };
+  }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.symbol !== prevProps.symbol){
-            this.getStatsData();
-        }
+  componentDidUpdate(prevProps) {
+    if (this.props.symbol !== prevProps.symbol) {
+      this.getStatsData();
     }
+  }
 
-    getStatsData = () => {
-        const symbol = this.props.symbol;
-        getKeyStats(symbol).then(keyStats => this.setState({ keyStats: keyStats }))
-    }
+  getStatsData = () => {
+    const symbol = this.props.symbol;
+    getKeyStats(symbol).then(keyStats => this.setState({ keyStats: keyStats }));
+  };
 
-    render() {
-        const { keyStats } = this.state;
-        return (
-            keyStats === null ? null : <Stats stats={keyStats} />
-        );   
-    }
+  render() {
+    const { keyStats } = this.state;
+    return keyStats === null ? null : <Stats stats={keyStats} />;
+  }
 }
 
 const Stats = ({ stats }) => (
-    <div>
-        <span>Previous Close: </span><span>{stats.previousClose}</span>
-        <br/>
-        <span>Day Range: </span><span>{stats.dayRange}</span>
-        <br/>
-        <span>Volume: </span><span>{stats.volume}</span>
-        <br/>
-        <span>Market Cap: </span><span>{stats.marketCap}</span>
-        <br/>
-        <span>P/E Ratio: </span><span>{stats.peRatio}</span>
-        <br/>
-        <span>Open: </span><span>{stats.open}</span>
-        <br/>
-        <span>52 Week Range: </span><span>{stats.weekRange52}</span>
-        <br/>
-        <span>Total Avg. Volume: </span><span>{stats.avgTotalVolume}</span>
-        <br/>
-        <span>Earnings Per Share: </span><span>{stats.earningsPerShare}</span>
-        <br/>
-        <span>Dividend  Yield: </span><span>{stats.dividendYield}</span>
-        <br/>
-    </div>
+  <div>
+    <span>Previous Close: </span>
+    <span>{stats.previousClose}</span>
+    <br />
+    <span>Day Range: </span>
+    <span>{stats.dayRange}</span>
+    <br />
+    <span>Volume: </span>
+    <span>{stats.volume}</span>
+    <br />
+    <span>Market Cap: </span>
+    <span>{stats.marketCap}</span>
+    <br />
+    <span>P/E Ratio: </span>
+    <span>{stats.peRatio}</span>
+    <br />
+    <span>Open: </span>
+    <span>{stats.open}</span>
+    <br />
+    <span>52 Week Range: </span>
+    <span>{stats.weekRange52}</span>
+    <br />
+    <span>Total Avg. Volume: </span>
+    <span>{stats.avgTotalVolume}</span>
+    <br />
+    <span>Earnings Per Share: </span>
+    <span>{stats.earningsPerShare}</span>
+    <br />
+    <span>Dividend Yield: </span>
+    <span>{stats.dividendYield}</span>
+    <br />
+  </div>
 );
 
 export default StatsContainer;
