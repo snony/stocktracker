cb00e0ed1027d8cff611a0a5ddd3f4d0b9f33850
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-09-17 11:57:38 +0100

    removed commented out code

--- src/components/company/overview.js
+++ src/components/company/overview.js
@@ -2,35 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { mapStateToProps } from '../../redux/index'
 
-/*
-class OverViewContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            overview: null,
-        }
-    }
-
-    getOverviewData = () => {
-        const symbol = this.props.symbol;
-        getOverview(symbol).then(overviewData => this.setState({ overview: overviewData }));
-    }
-
-    componentDidUpdate(prevProps) {
-        if (this.props.symbol !== prevProps.symbol) {
-            this.getOverviewData();
-        }
-    }
-
-    render() {
-        const overview = this.state.overview;
-        return (
-            overview === null ? null : <AboutCompany overview={overview} />
-        );
-    }
-}
-*/
-
 const AboutCompany = ({ companyInfo: { overview } }) => (
   <div>
     {overview.companyName} {overview.symbol}
