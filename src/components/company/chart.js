a9981349056351388bf9b1a152d5640fe8fe915f
Toby Li <toby.li@weareadaptive.com>
2018-09-19 12:04:04 +0100

    Took Logo component into its own file and moved chart container styling to css

--- src/components/company/chart.js
+++ src/components/company/chart.js
@@ -114,7 +114,7 @@ const tickStyle = {
 
 const DisplayChart = ({ priceFilter, history }) => {
   return (
-    <ResponsiveContainer width="99%" height="100%">
+    <ResponsiveContainer>
       <AreaChart data={history}>
         <defs>
           <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
