927c51d999c51e98d186fdb5859217c175b46292
snony <42961027+snony@users.noreply.github.com>
2018-09-20 13:48:22 +0100

    Added Search icon and styled it (#13)
    * added search icon
    
    * added icon to search
    
    * This has now been added
    
    * Reorganized border bottom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import StockTracker from './stockTracker'
 
 import 'ress'
 import './index.css'
 import './colours.css'
+
+library.add(faSearch)
 
 ReactDOM.render(<StockTracker />, document.getElementById('root'))
