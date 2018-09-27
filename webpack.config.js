f141f432ed988c7f1cacde3929b897132d719264
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-09-27 11:46:36 +0100

    webpack is configured

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path')
+
+module.exports = {
+    entry: {
+        app:["./src/index.js"]
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/dist'
+        
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+    mode: "development",
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"],
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            {
+                test: /\.css$/,
+                use:[
+                  {
+                      loader: 'style-loader'
+                  },{
+                      loader: 'css-loader'
+                  }
+              ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+    watch: true
+};
