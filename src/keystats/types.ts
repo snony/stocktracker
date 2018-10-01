0f8db095e390068ce403b8cc59cb127875e9525b
ianchan035 <ian035@hotmail.com>
2018-10-01 17:55:39 +0100

    removed comment in type.ts

--- src/keystats/types.ts
+++ src/keystats/types.ts
@@ -2,18 +2,3 @@ import { extractData } from '../api'
 
 export type KeyStats = ReturnType<typeof extractData> 
 
-// export interface KeyStatsProps {
-//   earningsPerShare: number,
-//   dividendYield: number,
-//   previousClose: number,
-//   open: number,
-//   close: number,
-//   dayHigh: number,
-//   dayLow: number,
-//   volume: number,
-//   avgTotalVolume: number,
-//   peRatio: number,
-//   marketCap: number,
-//   week52High: number,
-//   week52Low: number
-// } 
