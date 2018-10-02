da97ac75977b223b2924107148843e3acbc736e4
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-10-02 11:14:05 +0100

    Sticked with SFC for now

--- src/stockTracker.tsx
+++ src/stockTracker.tsx
@@ -11,38 +11,34 @@ import AdaptiveLogo from './logo'
 
 const ComponentTitle = ({ title }: { title: string }) => <h3 className="component-title">{title}</h3>
 
-export class StockTracker extends React.Component<{}, {}>{
-
-  public render() {
-    return (
-      <div className="stock-tracker-container">
-        <div className="stock-tracker-container__logo">
-          <AdaptiveLogo />
-        </div>
-        <div className="stock-tracker-container__search">
-          <Search />
-        </div>
-        <div className="stock-tracker-container__history">
-          <History />
-        </div>
-        <div className="stock-tracker-container__news">
-          <ComponentTitle title="News" />
-          <News />
-        </div>
-        <div className="stock-tracker-container__key-stats">
-          <ComponentTitle title="Key Stats" />
-          <KeyStats />
-        </div>
-        <div className="stock-tracker-overview">
-          <ComponentTitle title="Overview" />
-          <Overview />
-        </div>
-        <div className="stock-tracker-container__peers">
-          <ComponentTitle title="Peers" />
-          <Peers />
-        </div>
-      </div>)
-  }
-}
+export const StockTracker: React.SFC = () => (
+  <div className="stock-tracker-container">
+    <div className="stock-tracker-container__logo">
+      <AdaptiveLogo />
+    </div>
+    <div className="stock-tracker-container__search">
+      <Search />
+    </div>
+    <div className="stock-tracker-container__history">
+      <History />
+    </div>
+    <div className="stock-tracker-container__news">
+      <ComponentTitle title="News" />
+      <News />
+    </div>
+    <div className="stock-tracker-container__key-stats">
+      <ComponentTitle title="Key Stats" />
+      <KeyStats />
+    </div>
+    <div className="stock-tracker-overview">
+      <ComponentTitle title="Overview" />
+      <Overview />
+    </div>
+    <div className="stock-tracker-container__peers">
+      <ComponentTitle title="Peers" />
+      <Peers />
+    </div>
+  </div>
+)
 
 export default StockTracker
