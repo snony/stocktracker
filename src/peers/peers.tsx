41c6ab2979924d1aa93bf916d5dafebe851205e5
ianchan035 <ian035@hotmail.com>
2018-10-03 17:53:51 +0100

    remoed a layer of nesting in peers state

--- src/peers/peers.tsx
+++ src/peers/peers.tsx
@@ -3,7 +3,7 @@ import { peersContainerState } from './peersContainer';
 
 type PeersProps = peersContainerState
 
-const Peers: React.SFC<PeersProps> = ({ peers: { peers } }) => {
+const Peers: React.SFC<PeersProps> = ({ peers }) => {
   const peersData = peers.join(', ')
 
   return <span className="label label--small">{peersData}</span>
