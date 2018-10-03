927d547fcf4a345ba436accafeb9f2bcb7e6f9f3
Toby Li <toby.li@weareadaptive.com>
2018-10-03 11:10:10 +0100

    Refactored overview actions to use action creators

--- src/overview/overviewActions.ts
+++ src/overview/overviewActions.ts
@@ -1,3 +1,4 @@
+import { ActionCreator } from 'redux'
 import { ThunkAction } from 'redux-thunk'
 
 import { API, GlobalState } from '../types'
@@ -5,12 +6,17 @@ import { OverviewState } from './types'
 
 export const OVERVIEW_RECEIVED_ACTION = 'OVERVIEW_RECEIVED_ACTION'
 
-const overviewReceivedAction = (overview: OverviewState) => ({
-  type: OVERVIEW_RECEIVED_ACTION as typeof OVERVIEW_RECEIVED_ACTION,
+export interface OverviewReceivedAction extends Action {
+  type: typeof OVERVIEW_RECEIVED_ACTION
+  overview: OverviewState
+}
+
+const overviewReceivedAction: ActionCreator<OverviewReceivedAction> = (
+  overview: OverviewState
+) => ({
+  type: OVERVIEW_RECEIVED_ACTION,
   overview
 })
-
-export type OverviewReceivedAction = ReturnType<typeof overviewReceivedAction>
 
 export const getOverviewData: (
   symbol: string
