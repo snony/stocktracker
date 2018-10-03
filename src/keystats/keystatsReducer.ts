6ae00137b11b487a7ffd093471bcbd90c154ce52
ianchan035 <ian035@hotmail.com>
2018-10-03 18:34:52 +0100

    redux reducer saved

--- src/keystats/keystatsReducer.ts
+++ src/keystats/keystatsReducer.ts
@@ -1,39 +1,19 @@
 import { Reducer } from 'redux'
+
 import { STATS_RECEIVED_ACTION, StatsReceivedAction } from './keystatsActions'
-import { KeyStats } from './types';
+import { KeyStats } from './types'
 
-interface KeyStatsState {
-  keystats: KeyStats
-}
+type KeyStatsState = KeyStats | {} 
 
-const initialState: KeyStatsState = {
-  keystats: {
-    earningsPerShare: 0,
-    dividendYield: 0,
-    previousClose: 0,
-    open: 0,
-    
-    dayHigh: 0,
-    dayLow: 0,
-    volume: 0,
-    avgTotalVolume: 0,
-    peRatio: 0,
-    marketCap: 0,
-    week52High: 0,
-    week52Low: 0
-  }
-}
+const initialState: KeyStatsState = {}
 
 const keystats: Reducer<KeyStatsState, StatsReceivedAction> = (
-  state = initialState, 
+  state = initialState,
   action
-  ) => {
+) => {
   switch (action.type) {
     case STATS_RECEIVED_ACTION:
-      return {
-        ...state,
-        keystats: action.keystats
-      }
+      return { ...action.keystats }
     default:
       return state
   }
