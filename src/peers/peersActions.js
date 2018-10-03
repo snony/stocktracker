0805e2fdae5c391dd339408e747ee97d0c73c8c6
Ian Chan <ian035@hotmail.com>
2018-10-03 10:42:43 +0100

    AGP-119 peers-redux (#40)
    * typed redux peers
    
    * removed console.log

--- src/peers/peersActions.js
+++
@@ -1,9 +0,0 @@
-export const PEERS_RECEIVED_ACTION = 'PEERS_RECEIVED_ACTION'
-
-const peersReceivedAction = peers => ({ type: PEERS_RECEIVED_ACTION, peers })
-
-export const getPeersData = symbol => (dispatch, _, api) => {
-  api.getPeers(symbol).then(peers => {
-    return dispatch(peersReceivedAction(peers))
-  })
-}
