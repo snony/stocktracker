17d6f9a21d2b7981493ee7b09a580c5f61a2b91b
Toby Li <toby.li@weareadaptive.com>
2018-10-03 14:23:25 +0100

    Removed null check in overview component

--- src/overview/overview.tsx
+++ src/overview/overview.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 
 import { OverviewContainerStateProps } from './overviewContainer'
 
-const Overview: React.SFC<OverviewContainerStateProps> = ({ overview }) =>
-  overview === null ? null : (
-    <div className="overview-container">
-      <span className="label label--big">
-        {overview.companyName} ({overview.symbol})
-      </span>
-      <a href={overview.website} className="label label--small label--link">
-        {overview.website}
-      </a>
-      <span className="label">{overview.description}</span>
-    </div>
-  )
+const Overview: React.SFC<OverviewContainerStateProps> = ({ overview }) => (
+  <div className="overview-container">
+    <span className="label label--big">
+      {overview.companyName} ({overview.symbol})
+    </span>
+    <a href={overview.website} className="label label--small label--link">
+      {overview.website}
+    </a>
+    <span className="label">{overview.description}</span>
+  </div>
+)
 
 export default Overview
