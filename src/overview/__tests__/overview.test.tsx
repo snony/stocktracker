de9e7ad48d4b595ba1be327b84e6ee30d24b4119
Toby Li <toby.li@weareadaptive.com>
2018-10-05 15:12:24 +0100

    Removed unneeded imports and dependencies

--- src/overview/__tests__/overview.test.tsx
+++ src/overview/__tests__/overview.test.tsx
@@ -1,12 +1,8 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
 import renderer from 'react-test-renderer'
 
 import Overview from '../overview'
 import { mockAaplOverviewData } from './mockData'
-
-Enzyme.configure({ adapter: new Adapter() })
 
 describe('overview component', () => {
   it('should render correctly', () => {
