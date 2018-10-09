413ea58fbc44cf27fe8b1812dd74331af84e38cd
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-10-09 09:15:38 +0100

    added unit test for querySymbols

--- src/search/__tests__/querySymbols.test.ts
+++ src/search/__tests__/querySymbols.test.ts
@@ -1,9 +1,15 @@
+import { Company } from '../../types';
 import QuerySymbols from '../querySymbols'
 
 
 describe('QuerySymbols', () => {
     it('should return empty if empty searchString is passed to it', () => {
         expect(QuerySymbols('', [])).toEqual([]);
+    })
+
+    it('should return companies matching the searchString', () => {
+        const companies: Company[] = [{ name: 'Apple Inc', symbol: 'aapl' }, { name: 'Alphabet Inc', symbol: 'Googl' }, { name: 'Faceboon Inc', symbol: 'Inc' }]
+        expect(QuerySymbols('aapl', companies)).toEqual([{ name: 'Apple Inc', symbol: 'aapl' }, { "name": "Alphabet Inc", "symbol": "Googl" }])
     })
 
 })
