e4b03e1e1980417d01ca300abb196e34c05f1016
ianchan035 <ian035@hotmail.com>
2018-10-10 16:44:05 +0100

    using async/await for tests

--- src/peers/__test__/peersActions.test.ts
+++ src/peers/__test__/peersActions.test.ts
@@ -18,25 +18,25 @@ describe('test for peers action', () => {
     store = generateMockStore({ peers: mockPeersData.emptyPeers }, api)
     store.clearActions()
   })
-  
+
   it('should create new PEERS_RECEIVED_ACTION object', () => {
     const action = peersReceivedAction(mockPeersData.examplePeers)
     expect(action).toEqual(expectedAction)
   })
 
-  it('should dispatch PEERS_RECEIVED_ACTION when the fetch is successful', () => {
+  it('should dispatch PEERS_RECEIVED_ACTION when the fetch is successful', async () => {
     const mockDispatch = store.dispatch
-    mockDispatch<any>(getPeersData(mockSymbol)).then(() => {
-        const mockStoreAction = store.getActions()
-        expect(mockStoreAction[0] as object[]).toEqual(expectedAction)
-      })
+
+    await mockDispatch<any>(getPeersData(mockSymbol))
+    const mockStoreAction = store.getActions()
+    expect(mockStoreAction[0] as object[]).toEqual(expectedAction)
   })
 })
 
-describe('test for peers action without mock-redux-store', () => {
+describe('test for peers action without mock-redux-store', async () => {
   const mockDispatch = jest.fn()
   const result = getPeersData(mockSymbol)
-  return result(mockDispatch, null, api).then(() => {
-    expect(mockDispatch.mock.calls).toMatchSnapshot()
-  })
+  await result(mockDispatch, null, api)
+  
+  expect(mockDispatch.mock.calls).toMatchSnapshot()
 })
