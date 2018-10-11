ea7459dec8061f8e6c793fa134865d229cb82a4f
ianchan035 <ian035@hotmail.com>
2018-10-11 10:28:13 +0100

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-    "roots": [
-        "<rootDir>/src"
-    ],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
     },
-    "setupFiles": ["<rootDir>/src/__test__/setupTest.ts"],
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
+    testRegex: '((/__tests__/)?(.*\\.)?(test|spec))\\.tsx?$',
+    moduleNameMapper: {
+        '\\.(css)$': 'identity-obj-proxy'
+    },
+    setupFiles: ["<rootDir>/src/__test__/setupTest.ts"],
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleDirectories: ['node_modules', 'src']
 }
