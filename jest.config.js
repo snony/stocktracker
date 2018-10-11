ff9e32bf42298016739a911e27533ed9790dfb8c
ianchan035 <ian035@hotmail.com>
2018-10-11 10:35:28 +0100

    conflict

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     moduleNameMapper: {
         '\\.(css)$': 'identity-obj-proxy'
     },
+    setupFiles: ["<rootDir>/src/__test__/setupTest.ts"],
     snapshotSerializers: ['enzyme-to-json/serializer'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     moduleDirectories: ['node_modules', 'src']
