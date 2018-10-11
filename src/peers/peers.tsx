62a968cee90447b2482dd4cf651f9603b008ed80
ianchan035 <ian035@hotmail.com>
2018-10-11 09:57:32 +0100

    Revert "fixed if else clause and added some more async/await"
    This reverts commit 346d8e0f40debe2eb2ebe091faba2752762e6482.

--- src/peers/peers.tsx
+++ src/peers/peers.tsx
@@ -8,9 +8,9 @@ const Peers: React.SFC<PeersProps> = ({ peers }) => {
   if(peers.length !== 0) {
     const peersData = peers.join(', ')
     return <span className="label label--small">{peersData}</span>
-  } else {
-    return <p className="label label--small">Loading...</p>
   }
+
+  return <p className="label label--small">Loading...</p>
 }
 
 export default Peers
