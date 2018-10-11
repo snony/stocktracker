f749226f3e1412bc5a7f37da5bca472f4027aad1
Toby Li <toby.li@weareadaptive.com>
2018-10-11 10:42:18 +0100

    Spelling mistake

--- src/__tests__/bootstrapReducer.test.ts
+++ src/__tests__/bootstrapReducer.test.ts
@@ -2,19 +2,18 @@ import { getCompanySymbols } from '../__mock__/companySymbols.mock'
 import { COMPANY_SYMBOLS_RECEIVED_ACTION, CompanySymbolsReceiveAction } from '../bootstrap'
 import bootstrapReducer from '../bootstrapReducer'
 
-
 describe('Bootstrap Reducer', () => {
-    it('should return initial state', () => {
-        const companySymbolAaction = {} as CompanySymbolsReceiveAction
-        expect(bootstrapReducer([], companySymbolAaction)).toEqual([])
-    })
+  it('should return initial state', () => {
+    const companySymbolAction = {} as CompanySymbolsReceiveAction
+    expect(bootstrapReducer([], companySymbolAction)).toEqual([])
+  })
 
-    it('should handle company symbol receive action', () => {
-        const companySymbols = getCompanySymbols(10)
-        const companyReceiveAction: CompanySymbolsReceiveAction = {
-            type: COMPANY_SYMBOLS_RECEIVED_ACTION,
-            companySymbols
-        }
-        expect(bootstrapReducer([], companyReceiveAction)).toEqual(companySymbols)
-    })
+  it('should handle company symbol receive action', () => {
+    const companySymbols = getCompanySymbols(10)
+    const companyReceiveAction: CompanySymbolsReceiveAction = {
+      type: COMPANY_SYMBOLS_RECEIVED_ACTION,
+      companySymbols
+    }
+    expect(bootstrapReducer([], companyReceiveAction)).toEqual(companySymbols)
+  })
 })
