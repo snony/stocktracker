960a420e57389c1183950bd731f163820a0ed571
snony <42961027+snony@users.noreply.github.com>
2018-10-11 09:16:10 +0100

    Created Default Test for News (#49)
    * created default tests for news
    
    * added mock for api and news instead of using fetch-mock
    
    * changed to async and await

---
+++ src/news/__tests__/newsActions.test.ts
@@ -0,0 +1,46 @@
+import configureMockStore from 'redux-mock-store'
+import { ThunkDispatch } from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk'
+import { API, GlobalState } from 'types';
+import api from '../__mocks__/api.mock';
+import { getMockNews } from '../__mocks__/news';
+import { getNewsData, NEWS_RECEIVED_ACTION, NewsReceivedAction, receiveNewsAction } from '../newsActions'
+
+
+describe('actions', () => {
+
+    describe('synchronous actions', () => {
+        it('receiveNewsAction should create a news received action', () => {
+            const newsItems = getMockNews();
+            const expectedAction = {
+                type: NEWS_RECEIVED_ACTION,
+                newsItems
+            }
+
+            expect(receiveNewsAction(newsItems)).toEqual(expectedAction)
+        })
+    })
+
+    describe('asynchronous actions', () => {
+        it('getNewsData should  dispatch an async news received action', async () => {
+            type ThunkDispatchNewsReceivedAction = ThunkDispatch<GlobalState, API, NewsReceivedAction>
+            type Store = GlobalState | ThunkDispatchNewsReceivedAction
+            const newsData = getMockNews()
+            const expectedAction = [{
+                type: NEWS_RECEIVED_ACTION,
+                newsItems: newsData
+            }]
+
+            // Setup the mock store
+            const middlewares = [thunkMiddleware.withExtraArgument(api)]
+            const mockStore: Store = configureMockStore(middlewares)
+            const store = mockStore(jest.fn())
+
+            await store.dispatch(getNewsData('aapl'))
+            expect(store.getActions()).toEqual(expectedAction)
+
+        })
+    })
+
+})
+
