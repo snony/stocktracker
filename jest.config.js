6b6006c6101c5be34d73150e41dd2144be19a72e
snony <42961027+snony@users.noreply.github.com>
2018-10-11 09:24:37 +0100

    Agp 99 search (#52)
    * setup files associated with testing
    
    * test for results is done
    
    * some default tests for search
    
    * added unit test for querySymbols
    
    * changed value
    
    * double checked the test
    
    * removed unnecessary declaration
    
    * removed optional tsx
    
    * mocked company
    
    * resolved issues
    
    * added a partial function instead
    
    * improved code quality
    
    * improved code quality
    
    * was done
    
    * removed duplication of globalstate

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '((/__tests__/)?(.*\\.)?(test|spec))\\.tsx?$',
+  moduleNameMapper: {
+    '\\.(css)$': 'identity-obj-proxy'
+  },
+  snapshotSerializers: ['enzyme-to-json/serializer'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleDirectories: ['node_modules', 'src']
 }
---
+++ src/search/__tests__/querySymbols.test.ts
@@ -0,0 +1,12 @@
+import { Company } from '../../types';
+import QuerySymbols from '../querySymbols'
+
+
+describe('QuerySymbols', () => {
+
+    it('should return companies matching the searchString', () => {
+        const companies: Company[] = [{ name: 'Apple Inc', symbol: 'aapl' }, { name: 'Alphabet Inc', symbol: 'googl' }, { name: 'Facebook Inc', symbol: 'fb' }]
+        expect(QuerySymbols('aapl', companies)).toEqual([{ name: 'Apple Inc', symbol: 'aapl' }, { "name": "Alphabet Inc", "symbol": "googl" }])
+    })
+
+})
