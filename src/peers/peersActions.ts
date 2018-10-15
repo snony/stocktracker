5a9fffb6a5e078b1eb65c5873c1761f1d25c2a56
ianchan035 <ian035@hotmail.com>
2018-10-15 17:54:39 +0100

    added error handling and error action creator to peers

--- src/peers/peersActions.ts
+++ src/peers/peersActions.ts
@@ -1,28 +1,48 @@
-import { Action, ActionCreator } from 'redux'
+import { Action, ActionCreator, ActionCreatorsMapObject } from 'redux'
 import { ThunkAction } from 'redux-thunk'
 import { API, GlobalState } from 'types'
 
 import { Peers } from './types'
 
-export const PEERS_RECEIVED_ACTION = 'PEERS_RECEIVED_ACTION'
+export enum PEERS_ACTION_TYPES {
+  RECEIVED_DATA = 'PEERS_RECEIVED_DATA',
+  RECEIVED_ERROR = 'PEERS_RECEIVED_ERROR'
+}
 
-export interface PeersReceivedAction extends Action {
-  type: typeof PEERS_RECEIVED_ACTION
+export interface PeersReceivedData extends Action {
+  type: PEERS_ACTION_TYPES.RECEIVED_DATA
   peers: Peers
 }
 
-export const peersReceivedAction: ActionCreator<PeersReceivedAction> = (peers: string[]) => ({
-  type: PEERS_RECEIVED_ACTION,
+const receivedData: ActionCreator<PeersReceivedData> = (peers: string[]) => ({
+  type: PEERS_ACTION_TYPES.RECEIVED_DATA,
   peers
 })
 
-type ThunkResult<R> = (symbol: string) => ThunkAction<R, GlobalState, API, PeersReceivedAction>
+export interface PeersReceivedError extends Action {
+  type: PEERS_ACTION_TYPES.RECEIVED_ERROR,
+  error: boolean
+}
 
-export const getPeersData: ThunkResult<any> = (symbol: string) => async (dispatch, _, api) => {
+const peersReceivedError: ActionCreator<PeersReceivedError> = () => ({
+  type: PEERS_ACTION_TYPES.RECEIVED_ERROR,
+  error: true
+})
+
+type PeersActions = PeersReceivedData | PeersReceivedError
+
+export const PeersAction: ActionCreatorsMapObject<PeersActions> = {
+  data: receivedData,
+  error: peersReceivedError
+}  
+
+type ThunkResult<R> = (symbol: string) => ThunkAction<R, GlobalState, API, PeersActions>
+
+export const getPeersData: ThunkResult<void> = (symbol: string) => async (dispatch, _, api) => {
   try {
     const peers = await api.getPeers(symbol)
-    dispatch(peersReceivedAction(peers))
+    dispatch(PeersAction.data(peers))
   } catch {
-    console.log('Error')
+    dispatch(PeersAction.error())
   }
 }
