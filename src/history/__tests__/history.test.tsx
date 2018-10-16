465ecf97e259d26c27f1f2d41856419c51b76260
Toby Li <toby.li@weareadaptive.com>
2018-10-16 11:19:31 +0100

    Added snapshot tests for history component under different fetch status

--- src/history/__tests__/history.test.tsx
+++ src/history/__tests__/history.test.tsx
@@ -1,17 +1,24 @@
 import { mockGlobalState } from '__mock__/globalstate.mock'
 import { mount, shallow } from 'enzyme'
+import fetchStatus from 'fetchStatus'
 import History, { HistoryChart } from 'history/history'
 import { HistoryContainerProps } from 'history/historyContainer'
+import { initialState } from 'history/historyReducer'
 import { HistoryChartProps } from 'history/types'
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-const mockHistoryContainerProps: HistoryContainerProps = {
+const historyContainerPropsCreator = (
+  overrides: Partial<HistoryContainerProps>
+): HistoryContainerProps => ({
   company: 'AAPL',
-  ...mockGlobalState.history,
   onClickFilterHistoryByDate: jest.fn(),
-  onClickFilterHistoryByPrice: jest.fn()
-}
+  onClickFilterHistoryByPrice: jest.fn(),
+  ...initialState,
+  ...overrides
+})
+
+const mockHistoryContainerProps = historyContainerPropsCreator(mockGlobalState.history)
 
 const createNodeMock = () => {
   const doc = document.implementation.createHTMLDocument()
@@ -31,6 +38,15 @@ describe('history component', () => {
     expect(tree).toMatchSnapshot()
   })
 
+  it('should render correctly before initial fetch', () => {
+    const mockInitialHistoryContainerProps = historyContainerPropsCreator({})
+    const tree = renderer
+      .create(<History {...mockInitialHistoryContainerProps} />, { createNodeMock })
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
   it('should render correctly', () => {
     const tree = renderer
       .create(<History {...mockHistoryContainerProps} />, { createNodeMock })
@@ -39,15 +55,16 @@ describe('history component', () => {
     expect(tree).toMatchSnapshot()
   })
 
-  // it('should render correctly with no historical data', () => {
-  //   const mockEmptyHistoryContainerProps: HistoryContainerProps = {
-  //     ...mockHistoryContainerProps,
-  //     history: []
-  //   }
-  //   const wrapper = shallow(<History {...mockEmptyHistoryContainerProps} />)
+  it('should render correctly on data fetch failed', () => {
+    const mockFailedHistoryContainerProps = historyContainerPropsCreator({
+      fetchStatus: fetchStatus.FAILED
+    })
+    const tree = renderer
+      .create(<History {...mockFailedHistoryContainerProps} />, { createNodeMock })
+      .toJSON()
 
-  //   expect(wrapper.getElement()).toBeNull()
-  // })
+    expect(tree).toMatchSnapshot()
+  })
 
   it('should render filter buttons correctly', () => {
     const wrapper = shallow(<History {...mockHistoryContainerProps} />)
