bc1c218268d35e3c4b9a5e1edce0b53ad7e312b9
Toby Li <toby.li@weareadaptive.com>
2018-10-16 16:28:12 +0100

    Formats news headline over 2 lines long with ellipsis using new package

--- src/news/news.tsx
+++ src/news/news.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
+import Truncate from 'react-truncate'
 
 import { NewsItemsProps, NewsProps } from './types'
-
-// TODO ML 28/09, these were needed previously but cannot be imported in typescript cause of lack of
-// types. We have to think about how to handle this later.
-// import LinesEllipsis from 'react-lines-ellipsis'
-// import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC'
-// const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis)
-// {/* <ResponsiveEllipsis text={newsData.headline} maxLine="2" ellipsis="..." trimRight /> */}
 
 export const News: React.SFC<NewsProps> = ({ newsData }) => (
   <div>
     <a href={newsData.url} className="label label--link">
-      {newsData.headline}
+      <Truncate lines={2} trimWhitespace>
+        {newsData.headline}
+      </Truncate>
     </a>
     <span className="label label--small">
       {newsData.datetime} - {newsData.source}
