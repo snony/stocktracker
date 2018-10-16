b6c41a3da68bdeee7ec0a9ab82c095451e5fc86b
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-10-16 11:26:28 +0100

    used the global variables

--- src/news/__tests__/newsReducer.test.ts
+++ src/news/__tests__/newsReducer.test.ts
@@ -1,24 +1,20 @@
-import { NEWS_FETCH_FAILED, NEWS_RECEIVED_ACTION, NewsReceivedAction } from '../newsActions'
-import newsReducer from '../newsReducer'
-import { FetchStatus, News, NewsItemsProps } from '../types'
+import { mockNews } from '__mock__/news.mock'
+import { NEWS_FETCH_FAILED, NEWS_RECEIVED_ACTION, NewsReceivedAction } from 'news/newsActions'
+import newsReducer, { initState } from 'news/newsReducer'
+import { FetchStatus, NewsItemsProps } from 'news/types'
 
 describe('news container reducer', () => {
   it('should return the initial state', () => {
-    const initState: NewsItemsProps = { newsItems: [], fetchStatus: FetchStatus.PENDING }
     expect(newsReducer(initState, {} as NewsReceivedAction)).toEqual(initState)
   })
 
   it('should handle NEWS_RECEIVED_ACTION', () => {
-    const newsItems: News[] = [
-      { url: 'apple', headline: 'lang', datetime: '05/10/2018', source: 'Washington Post' }
-    ]
-    const expectedState: NewsItemsProps = { newsItems, fetchStatus: FetchStatus.SUCCESS }
-    expect(newsReducer({} as NewsItemsProps, { type: NEWS_RECEIVED_ACTION, newsItems })).toEqual(expectedState)
+    const expectedState = mockNews
+    expect(newsReducer(initState, { type: NEWS_RECEIVED_ACTION, newsItems: mockNews.newsItems })).toEqual(expectedState)
   })
 
   it('should handle NEWS_FETCH_fAIL', () => {
-
-    const expectedState: NewsItemsProps = { fetchStatus: FetchStatus.FAIL } as NewsItemsProps
-    expect(newsReducer({} as NewsItemsProps, { type: NEWS_FETCH_FAILED })).toEqual(expectedState)
+    const expectedState: NewsItemsProps = { fetchStatus: FetchStatus.FAIL, newsItems: [] }
+    expect(newsReducer(initState, { type: NEWS_FETCH_FAILED })).toEqual(expectedState)
   })
 })
