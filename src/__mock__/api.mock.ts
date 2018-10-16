a25fcc892a50e9a6a561897a0d18c48a65796185
Toby Li <toby.li@weareadaptive.com>
2018-10-16 09:51:51 +0100

    Removed unused code in api.mock

--- src/__mock__/api.mock.ts
+++ src/__mock__/api.mock.ts
@@ -1,19 +1,4 @@
-import { Company } from 'types'
-
-import { getCompanySymbols as companySymbols } from './companySymbols.mock'
 import { mockGlobalState } from './globalstate.mock'
-
-const mockFetch = (data: object) =>
-  new Promise(resolve => {
-    return resolve(data)
-  })
-export const getCompanySymbols = () => {
-  const symbols: Company[] = companySymbols(25)
-
-  return mockFetch(symbols)
-}
-
-export default { getCompanySymbols }
 
 export const mockApi = {
   getCompanySymbols: jest.fn().mockResolvedValue(mockGlobalState.companySymbols),
