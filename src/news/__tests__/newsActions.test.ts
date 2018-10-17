18895fd27610149f20b72c7fe09831633fcda167
snony <42961027+snony@users.noreply.github.com>
2018-10-17 15:23:12 +0100

    Agp 223 news error (#62)
    * some changes to news
    
    * added errors to news and extended the tests
    
    * removed unnecessary comment
    
    * made sure to use global mock data
    
    * used the global variables

--- src/news/__tests__/newsActions.test.ts
+++ src/news/__tests__/newsActions.test.ts
@@ -1,37 +1,59 @@
-import { mockApi } from '__mock__/api.mock'
+import { mockApi, mockApiFailFetch } from '__mock__/api.mock'
 import { mockGlobalState } from '__mock__/globalstate.mock'
 import { generateMockStore } from '__mock__/mockStore.mock'
+import { mockNews } from '__mock__/news.mock'
 import {
   getNewsData,
+  getNewsFetchFailedAction,
+  NEWS_FETCH_FAILED,
   NEWS_RECEIVED_ACTION,
   NewsReceivedAction,
   receiveNewsAction
 } from 'news/newsActions'
+import { FetchStatus } from 'news/types'
 import { MockStore } from 'redux-mock-store'
 
 describe('actions', () => {
-  let store: MockStore<{}>
-  const newsItems = mockGlobalState.news
-  const expectedAction: NewsReceivedAction = {
-    type: NEWS_RECEIVED_ACTION,
-    newsItems
-  }
-
-  beforeEach(() => {
-    store = generateMockStore(mockGlobalState, mockApi)
-    store.clearActions()
-  })
 
   describe('synchronous actions', () => {
     it('receiveNewsAction should create a news received action', () => {
-      expect(receiveNewsAction(newsItems)).toEqual(expectedAction)
+      const expectedAction: NewsReceivedAction = {
+        type: NEWS_RECEIVED_ACTION,
+        newsItems: mockNews.newsItems
+      }
+      expect(receiveNewsAction(expectedAction.newsItems)).toEqual(expectedAction)
+    })
+
+    it('getNewsFetchFailedAction should create a news fetch fail action', () => {
+      expect(getNewsFetchFailedAction()).toEqual({ type: NEWS_FETCH_FAILED })
     })
   })
 
   describe('asynchronous actions', () => {
-    it('getNewsData should  dispatch an async news received action', async () => {
-      await store.dispatch(getNewsData('aapl') as any)
-      expect(store.getActions()).toEqual([expectedAction])
+    let store: MockStore<{}>
+    afterEach(() => {
+      store.clearActions()
+    })
+    it('getNewsData should dispatch an async news received action', async () => {
+      const expectedAction = [
+        {
+          type: NEWS_RECEIVED_ACTION,
+          newsItems: {
+            newsItems: mockNews.newsItems,
+            fetchStatus: FetchStatus.SUCCESS
+          }
+        }
+      ]
+
+      store = generateMockStore(mockGlobalState, mockApi)
+      await store.dispatch<any>(getNewsData('aapl'))
+      expect(store.getActions()).toEqual(expectedAction)
+    })
+
+    it('getNewsData should dispatch an async news fetch fail action', async () => {
+      store = generateMockStore(mockGlobalState, mockApiFailFetch)
+      await store.dispatch<any>(getNewsData('aapl'))
+      expect(store.getActions()).toEqual([{ type: NEWS_FETCH_FAILED }])
     })
   })
 })
