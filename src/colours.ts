76f6a1c9007b6a158d89015d676deefe0f6a8d85
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-10-19 16:56:22 +0100

    can now infer type for root components

--- src/colours.ts
+++ src/colours.ts
@@ -17,10 +17,10 @@ const theme = {
   ...colours,
   bg: `linear-gradient(127deg, ${colours.bgStartColour}, ${colours.bgMiddleColour}, 69%, ${
     colours.bgEndColour
-  })`,
+    })`,
   bgSearch: `linear-gradient(to bottom, ${colours.bgSearchStartColour}, ${
     colours.bgSearchEndColour
-  })`,
+    })`,
   heading: `solid 0.1rem ${colours.primaryColour}`,
   searchBarHeading: `solid 0.1rem ${colours.searchBarColour}`,
   separator: `solid 0.1rem ${colours.separatorColour}`
