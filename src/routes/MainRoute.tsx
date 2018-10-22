2cd0a9f4648374cb306b444af5a0629df0b3248b
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-10-22 12:56:47 +0100

    added the router for news

--- src/routes/MainRoute.tsx
+++ src/routes/MainRoute.tsx
@@ -1,14 +1,24 @@
-import React from'react'
+import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import KeyStats from 'keystats'
+import News from 'news'
 import StockTracker from 'stockTracker'
+import styled from 'styled'
+
+const DivContainer = styled('div')(props => `
+  display: grid;
+  grid-gap: 1.2rem;
+  grid-template-columns: 1;
+  background-image: ${props.theme.bg};
+`)
 
 const MainRoute: React.SFC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={StockTracker} />
-      <Route path="/keystats" component={KeyStats} />
+      <Route path="/keystats" render={() => <DivContainer><KeyStats /></DivContainer>} />
+      <Route path="/news" render={() => <DivContainer><News /></DivContainer>} />
     </Switch>
   </BrowserRouter>
 )
