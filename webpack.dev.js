88dcbfce11319c902fc14d52bb7f335394f7359e
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-10-24 12:48:05 +0100

    added the output and input for the entry of production

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-
+const webpack = require('webpack')
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'cheap-eval-source-map',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
     devServer: {
         contentBase: './public',
         hot: true,
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,20 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path')
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'cheap-source-map',
+    entry: {
+        client: ['./src/index.tsx'],
+        vendor: ['react', 'react-dom', 'redux'],
+    },
+    output: {
+        filename: '[name].[hash].bundle.js',
+        chunkFilename: '[name].[hash].bundle.js',
+        path: path.join(__dirname, 'dist'),
+    },
     optimization: {
         minimizer: [
             new UglifyJSPlugin({
