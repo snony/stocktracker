aab88477c2dd0bd1e0894848bf189857ec05e0dc
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-10-24 11:00:32 +0100

    added minimizer with uglifyJSplugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,40 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'cheap-module-source-map'
+    devtool: 'cheap-module-source-map',
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                sourceMap: true,
+                uglifyOptions: {
+                    compress: {
+                        inline: false
+                    }
+                }
+            })
+        ],
+        runtimeChunk: false,
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendor_app',
+                    chunks: 'all',
+                    minChunks: 2
+                }
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.bundle\.js$/,
+                use: 'bundle-loader'
+            }
+        ]
+    }
 })
