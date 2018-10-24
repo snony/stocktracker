a69668e2343b3a37dbd5232655f6c8fb9d8ddfd8
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2018-10-24 10:34:44 +0100

    removing unnecessary code

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path')
-const buildPath = path.join(__dirname, 'dist');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'cheap-module-source-map',
-
+    devtool: 'cheap-module-source-map'
 })
